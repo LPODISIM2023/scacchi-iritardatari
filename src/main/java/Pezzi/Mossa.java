0f38d5fa08fc4b8f9bebb720fa468a610024f91e
Francesco <francescovampa@gmail.com>
2024-03-09 12:16:44 +0100

    Aggiunta classi
    Aggiunta 'pezzo' e 'mossa'

---
+++ src/main/java/Pezzi/Mossa.java
@@ -0,0 +1,6 @@
+package Pezzi;
+
+public class Mossa {
+
+
+}
---
+++ src/main/java/Pezzi/Pezzo.java
@@ -0,0 +1,12 @@
+package Pezzi;
+
+import java.util.List;
+
+public abstract class Pezzo {
+
+    private String nome;
+
+
+    public abstract List<Mossa> getMosse();
+
+}
