70058a400c0087e5b2f22ccfa84774e78b7f5653
GiammarcoUbaldi <ubaldigiammarco19@gmail.com>
2024-03-28 00:52:38 +0100

    Immagine all'alfiere
    Scordato l'alfiere

--- src/main/java/Pezzi/Alfiere.java
+++ src/main/java/Pezzi/Alfiere.java
@@ -7,6 +7,7 @@ public class Alfiere extends Pezzo {
     //metodo costrutore classe Alfiere: crea una nuova istanza di Alfiere
     public Alfiere(String nome, String codice, int valore, boolean colore, int riga, int colonna){
         super(nome, codice, valore, colore, riga, colonna);
+        setImage();
     }
 
     @Override
